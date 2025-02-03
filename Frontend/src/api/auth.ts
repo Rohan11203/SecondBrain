c23170ec0db345d28d93436153e8c690cc44cdd6
Rohan <rohanshikhare410@gmail.com>
2025-02-03 21:37:04 +0530

    ..

--- Frontend/src/api/auth.ts
+++ Frontend/src/api/auth.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { AxiosHeaders } from 'axios';
 import { RegisterationData } from '../pages/Register';
 
 export async function OnRegister(data: RegisterationData){
@@ -10,5 +10,15 @@ export async function OnLogin(data: RegisterationData){
 }
 
 export async function AddContent(data:any){
-  return await axios.post('http://localhost:3000/api/v1/content',data)
+  const headers = {
+    Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NzkyMjFiNzc4M2NkOTEzOGI2ZGQ0NWEiLCJpYXQiOjE3Mzg1OTgxMDd9.LHpDrfjfBE4v1FM6Azw3nVeRg3ilyb4OFHhVnL1TEcc'
+  };
+  return await axios.post('http://localhost:3000/api/v1/content',data,{headers})
+}
+
+export async function GetContent(){
+  const headers = {
+    Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NzkyMjFiNzc4M2NkOTEzOGI2ZGQ0NWEiLCJpYXQiOjE3Mzg1OTgxMDd9.LHpDrfjfBE4v1FM6Azw3nVeRg3ilyb4OFHhVnL1TEcc'
+  };
+  return await axios.get('http://localhost:3000/api/v1/content',{headers})
 }
