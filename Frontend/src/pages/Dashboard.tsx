864a7febe34b0176dd4807332c326295d61b424d
Rohan <rohanshikhare410@gmail.com>
2025-02-03 21:36:47 +0530

    added api for add-content in frontend

--- Frontend/src/pages/Dashboard.tsx
+++ Frontend/src/pages/Dashboard.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { AddContent } from "../api/auth"
+import { AddContent, GetContent } from "../api/auth"
 
 export const Dashboard = () => {
   const [data, setData] = useState({
@@ -7,23 +7,27 @@ export const Dashboard = () => {
     type: "",
     link: "",
   })
-  async function handleSubmit(){
+  async function handleClick(){
     await AddContent(data)
     console.log(data)
   }
   const handleOnChange =(e:any) => {
     setData({...data, [e.target.name]: e.target.value});
   }
+
+  async function onClick (){
+    await GetContent();
+  }
   return ( 
     <div className="">
      <div>
-      <form onSubmit={handleSubmit}>
         <input type="text" placeholder="Title" value={data.title} name="title" onChange={handleOnChange} />
         <input type="text" placeholder="Type" value={data.type} name="type" onChange={handleOnChange}/>
         <input type="text" placeholder="Link" value={data.link} name="link" onChange={handleOnChange}/>
-        <input type="submit" placeholder="Submit"/>
-      </form>
+        <button onClick={handleClick} >Add content</button>
      </div>
+
+     <button onClick={onClick}>Get</button>
     </div>
   )
 }
